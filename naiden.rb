f61c482ee313ec48dd22c1f6191ea8a5e93fde30
Ash <161075247+zararashigal@users.noreply.github.com>
2024-06-05 05:29:54 +0200

    Create naiden.rb

---
+++ naiden.rb
@@ -0,0 +1,38 @@
+# Inishyalizayshun (lul)
+def Init (args)
+
+    # Define function table...
+
+    # We parse args!
+    Pargs(args)
+
+end
+
+###    DEFINITIONS. Think of them as vocabulary...    ###
+
+def Number (modifier)
+
+    # TODO: CONTINUE AFTER WORK...
+
+end
+
+###    NORMAL CODE STARTS HERE AGAIN!!                ###
+
+# Parse Args. uwu
+def Pargs (args)
+
+    # Argument handling.
+    what  = ARGV[0] # What we seek...
+    where = ARGV[1] # Where we seek it...
+
+    # Handle the what! (Opens concerts for the who, btw. Get it?)
+    HandleArgs(args, what, where)
+
+end
+
+# Handle Args.
+def HandleArgs (args, what, where)
+
+    # TODO: CONTINUE AFTER WORK...
+
+end
